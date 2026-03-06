41ca852fa204c85a07f18b7ad58ecb324f9269ef
audu0132ar <audumbarmore23@gmail.com>
2026-03-06 13:31:53 +0530

    update

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -11,6 +11,12 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+        "@components": path.resolve(__dirname, "./src/components"),
+        "@hooks": path.resolve(__dirname, "./src/hooks"),
+        "@lib": path.resolve(__dirname, "./src/lib"),
+        "@assets": path.resolve(__dirname, "./src/assets"),
+         "@config": path.resolve(__dirname, "./src/config")
+         
     },
   },
 })
