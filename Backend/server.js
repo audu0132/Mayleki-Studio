39403b0c436039007a412bba96db83fb36c16fe9
audu0132ar <audumbarmore23@gmail.com>
2026-07-12 23:22:48 +0530

    feat: initialize express server with database connection, CORS configuration, and centralized routing

--- Backend/server.js
+++ Backend/server.js
@@ -17,6 +17,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
+    "http://localhost:5174",
     "https://mayleki-studio.vercel.app"
   ],
   methods: ["GET","POST","PUT","DELETE","OPTIONS"],
