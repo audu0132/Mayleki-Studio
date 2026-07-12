962ec28e8f4c3eeb9aba1373a77fb891a6e0e6eb
audu0132ar <audumbarmore23@gmail.com>
2026-07-12 23:39:58 +0530

    feat: add JWT authentication middleware to protect routes

--- Backend/middleware/protect.js
+++ Backend/middleware/protect.js
@@ -5,9 +5,11 @@ const router = express.Router();
 const protect = (req, res, next) => {
   let token = req.headers.authorization;
 
-  if (token && token.startsWith("Bearer")) {
-    try {
+  if (token) {
+    if (token.startsWith("Bearer ")) {
       token = token.split(" ")[1];
+    }
+    try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.admin = decoded;
       next();
